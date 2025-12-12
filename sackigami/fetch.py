7a56874b677e5da84c8aeb0620ba9dd811f8286c
rednokay <207755534+rednokay@users.noreply.github.com>
2025-12-12 16:35:14 +0100

    feat: add basic data fetching and parsing
    Add basic data fetching and parsing functionality using nflreadpy
    and polars.
    Tests are included.

---
+++ sackigami/fetch.py
@@ -0,0 +1,123 @@
+from typing import Optional
+
+import nflreadpy as nfl
+import polars as pl
+
+TEAMS = {
+    "ATL": "Atlanta Falcons",
+    "TB": "Tampa Bay Buccaneers",
+    "WAS": "Washington Commanders",
+    "BUF": "Buffalo Bills",
+    "CAR": "Carolina Panthers",
+    "BAL": "Baltimore Ravens",
+}
+
+DATA_OF_INTEREST = [
+    "team",
+    "season",
+    "week",
+    "opponent_team",
+    "sacks_suffered",
+    "sack_yards_lost",
+    "sack_fumbles",
+    "sack_fumbles_lost",
+]
+
+STAT_THRESHOLDS = {
+    "sacks_suffered": 5,
+    "sack_yards_lost": -25,
+    "sack_fumbles": 2,
+    "sack_fumbles_lost": 2,
+}
+
+
+def retrieve_complete_team_stats() -> pl.DataFrame:
+    return nfl.load_team_stats(seasons=True, summary_level="week")
+
+
+def retrieve_last_week(complete_team_stats: pl.DataFrame) -> pl.DataFrame:
+    last_season: int = complete_team_stats.select(pl.col("season").last()).item()
+    last_week: int = complete_team_stats.select(pl.col("week").last()).item()
+
+    return complete_team_stats.filter(
+        (pl.col("season") == last_season) & (pl.col("week") == last_week)
+    )
+
+
+def parse_sack_data(weekly_stat_lines: pl.DataFrame) -> pl.DataFrame:
+    return weekly_stat_lines.select([data for data in DATA_OF_INTEREST])
+
+
+def filter_by_threshold(stat_lines: pl.DataFrame) -> pl.DataFrame:
+    return stat_lines.filter(
+        (pl.col("sacks_suffered") >= STAT_THRESHOLDS["sacks_suffered"])
+        | (pl.col("sack_yards_lost") <= STAT_THRESHOLDS["sack_yards_lost"])
+        | (pl.col("sack_fumbles") >= STAT_THRESHOLDS["sack_fumbles"])
+        | (pl.col("sack_fumbles_lost") >= STAT_THRESHOLDS["sack_fumbles_lost"])
+    )
+
+
+def find_similar_stat_lines(
+    complete_team_stats: pl.DataFrame, stat_line: pl.DataFrame
+) -> Optional[dict]:
+    similar_lines: pl.DataFrame = complete_team_stats.filter(
+        (
+            pl.col("sacks_suffered")
+            == stat_line.select(pl.col("sacks_suffered").last()).item()
+        )
+        & (
+            pl.col("sack_yards_lost")
+            == stat_line.select(pl.col("sack_yards_lost").last()).item()
+        )
+        & (
+            pl.col("sack_fumbles")
+            == stat_line.select(pl.col("sack_fumbles").last()).item()
+        )
+        & (
+            pl.col("sack_fumbles_lost")
+            == stat_line.select(pl.col("sack_fumbles_lost").last()).item()
+        )
+    )
+
+    similar_lines = similar_lines.filter(
+        ~(
+            (pl.col("team") == stat_line.select(pl.col("team").last()).item())
+            & (pl.col("season") == stat_line.select(pl.col("season").last()).item())
+            & (pl.col("week") == stat_line.select(pl.col("week").last()).item())
+        )
+    )
+
+    count: int = similar_lines.height
+
+    if count == 0:
+        return None
+
+    week = similar_lines.select(pl.col("week").last()).item()
+    season = similar_lines.select(pl.col("season").last()).item()
+
+    return {
+        "count": count,
+        "week": week,
+        "season": season,
+    }
+
+
+# TODO: Pluralize
+def print_stat_line(stat_line: pl.DataFrame, complete_team_stats: pl.DataFrame) -> None:
+    data = {
+        item: stat_line.select(pl.col(item).last()).item() for item in DATA_OF_INTEREST
+    }
+
+    prev: Optional[dict] = find_similar_stat_lines(complete_team_stats, stat_line)
+
+    print(
+        f"The {TEAMS[data["team"]]} suffered {data["sacks_suffered"]} sacks in their game against the {TEAMS[data["opponent_team"]]} today. "
+        + f"This resulted in {abs(data["sack_yards_lost"])} yards lost. "
+        + f"{data["sack_fumbles"]} of those sacks were strip-sacks resulting in {data["sack_fumbles_lost"]} turnovers."
+    )
+
+    if prev is not None:
+        print(
+            f'No Sackigami! This stat line happened {prev["count"]} times before. '
+            + f'Most recently in week {prev["week"]} of {prev["season"]}.'
+        )
---
+++ tests/test_fetch.py
@@ -0,0 +1,132 @@
+import random
+
+import polars as pl
+import pytest
+
+from sackigami.fetch import (
+    TEAMS,
+    filter_by_threshold,
+    find_similar_stat_lines,
+    retrieve_last_week,
+)
+
+
+@pytest.fixture
+def complete_stats() -> pl.DataFrame:
+    season: list[int] = [
+        1999,
+        2000,
+        2013,
+        2013,
+        2025,
+        2025,
+        2025,
+    ]
+    week: list[int] = [
+        random.randint(1, 18),
+        random.randint(1, 18),
+        random.randint(1, 10),
+        random.randint(10, 18),
+        13,
+        16,
+        16,
+    ]
+
+    teams_short: list[str] = list(TEAMS.keys())
+    team: list[str] = [
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        "WAS",
+        "BUF",
+        "WAS",
+    ]
+    opponent_team = random.choices(teams_short, k=7)
+    opponent_team: list[str] = [
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        random.choices(teams_short)[0],
+        "CAR",
+        "BAL",
+    ]
+    sacks_suffered: list[int] = [
+        7,
+        2,
+        2,
+        6,
+        7,
+        7,
+        7,
+    ]
+    sack_yards_lost: list[int] = [
+        -45,
+        -12,
+        -13,
+        -20,
+        -45,
+        -45,
+        -45,
+    ]
+    sack_fumbles: list[int] = [
+        3,
+        random.randint(0, 3),
+        random.randint(0, 3),
+        random.randint(0, 3),
+        3,
+        3,
+        3,
+    ]
+    sack_fumbles_lost: list[int] = [
+        2,
+        random.randint(0, sack_fumbles[1] + 1),
+        random.randint(0, sack_fumbles[2] + 1),
+        random.randint(0, sack_fumbles[3] + 1),
+        2,
+        2,
+        2,
+    ]
+
+    return pl.DataFrame(
+        {
+            "season": season,
+            "week": week,
+            "team": team,
+            "opponent_team": opponent_team,
+            "sacks_suffered": sacks_suffered,
+            "sack_yards_lost": sack_yards_lost,
+            "sack_fumbles": sack_fumbles,
+            "sack_fumbles_lost": sack_fumbles_lost,
+        }
+    )
+
+
+def test_retrieves_last_week(complete_stats):
+    last_week = retrieve_last_week(complete_stats)
+
+    expected = pl.DataFrame(
+        {
+            "season": [2025, 2025],
+            "week": [16, 16],
+            "team": ["BUF", "WAS"],
+            "opponent_team": ["CAR", "BAL"],
+            "sacks_suffered": [7, 7],
+            "sack_yards_lost": [-45, -45],
+            "sack_fumbles": [3, 3],
+            "sack_fumbles_lost": [2, 2],
+        }
+    )
+
+    assert last_week.to_dicts() == expected.to_dicts()
+
+
+class TestFindSimilarStatLines:
+    def test_existing_similar_stat_linse(self, complete_stats):
+        relevant_last_week = filter_by_threshold(retrieve_last_week(complete_stats))
+
+        sim = find_similar_stat_lines(complete_stats, relevant_last_week)
+
+        assert sim is not None
+        assert list(sim.values()) == [3, 16, 2025]
