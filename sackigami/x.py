a505fdc64874241a544f857e9f18ba076d03e2a5
rednokay <207755534+rednokay@users.noreply.github.com>
2025-12-17 19:05:55 +0100

    refactor: move API cred to data class
    Move API credentials into a data class.
    Also add docstrings to the complete file.

--- sackigami/x.py
+++ sackigami/x.py
@@ -1,28 +1,53 @@
 import os
-
 import tweepy
-from dotenv import load_dotenv
 import requests
+from dotenv import load_dotenv
+from dataclasses import dataclass
 
 load_dotenv()
 
 
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+@dataclass(frozen=True)
+class APICred:
+    api_key = os.getenv("API_KEY")
+    """Public X API key"""
+
+    api_secret = os.getenv("API_SECRET")
+    """Secret X API key"""
+
+    access_token = os.getenv("ACCESS_TOKEN")
+    """Public X API access token"""
+
+    access_secret = os.getenv("ACCESS_SECRET")
+    """Secret X API acccess token"""
+
+
+cred: APICred = APICred()
 
 
 def connect_to_client() -> tweepy.Client:
+    """Connect to the tweepy/X client.
+
+    Returns:
+        tweepy.Client: The connected client.
+    """
     return tweepy.Client(
-        consumer_key=API_KEY,
-        consumer_secret=API_SECRET,
-        access_token=ACCESS_TOKEN,
-        access_token_secret=ACCESS_SECRET,
+        consumer_key=cred.api_key,
+        consumer_secret=cred.api_secret,
+        access_token=cred.access_token,
+        access_token_secret=cred.access_secret,
     )
 
 
 def post(text: str) -> requests.Response:
+    """Post a post on X.
+
+    Args:
+        text (str): Text to post.
+
+    Returns:
+        requests.Response: Response by the client.
+    """
     client: tweepy.Client = connect_to_client()
     response = client.create_tweet(text=text)
     return response
