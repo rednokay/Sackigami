7db780ad1ee35863fdfbc50c4694707cd7fe1d48
rednokay <207755534+rednokay@users.noreply.github.com>
2025-12-17 05:52:55 +0100

    refactor(x): pack into seperate function
    The client connection as well as the hello world are packed into
    seperate functions.

--- sackigami/x.py
+++ sackigami/x.py
@@ -2,7 +2,6 @@ import os
 
 import tweepy
 from dotenv import load_dotenv
-from tweepy.pagination import Response
 
 load_dotenv()
 
@@ -12,15 +11,19 @@ API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
-client = tweepy.Client(
-    consumer_key=API_KEY,
-    consumer_secret=API_SECRET,
-    access_token=ACCESS_TOKEN,
-    access_token_secret=ACCESS_SECRET,
-)
+
+def connect_to_client() -> tweepy.Client:
+    return tweepy.Client(
+        consumer_key=API_KEY,
+        consumer_secret=API_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_SECRET,
+    )
 
 
-response = client.create_tweet(text="Hello World!")
+if __name__ == "__main__":
+    client: tweepy.Client = connect_to_client()
+    response = client.create_tweet(text="Hello World!")
 
-print(f"{type(Response)=}")
-print("Tweet sent", response)
+    print(f"{type(response)=}")
+    print("Tweet sent", response)
