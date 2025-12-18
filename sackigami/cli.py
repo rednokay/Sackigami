bb9a5b76036b8358bf5252463bc351110960dcc8
rednokay <207755534+rednokay@users.noreply.github.com>
2025-12-18 07:26:06 +0100

    feat: add basic CLI

---
+++ sackigami/cli.py
@@ -0,0 +1,31 @@
+import typer
+import polars as pl
+from bot import retrieve_complete_team_stats, retrieve_week, loop_over_week
+
+APP_KWARGS: dict[str, str | bool] = {
+    "suggest_commands": True,
+    "no_args_is_help": True,
+}
+"""Kwargs used for typer constructor."""
+
+app = typer.Typer(**APP_KWARGS)
+"""Typer app."""
+
+
+# TODO: Option to post on X with disabling degbug mode
+@app.command()
+def gbg() -> None:
+    """Runs the game-by-game Sackigami!
+    """
+    print("Getting game data ...")
+    complete_stats: pl.DataFrame = retrieve_complete_team_stats()
+
+    print("Parse latest week and filter for relevancy ...")
+    last_week: pl.DataFrame = retrieve_week(complete_stats)
+
+    print("Looping over games")
+    loop_over_week(last_week, complete_stats)
+
+
+if __name__ == "__main__":
+    app()
