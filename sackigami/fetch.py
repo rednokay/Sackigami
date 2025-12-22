65e83338003173c6fe6b1a629e4a0ed65cd68b8b
rednokay <207755534+rednokay@users.noreply.github.com>
2025-12-22 21:50:53 +0100

    doc: add docstrings

--- sackigami/fetch.py
+++ sackigami/fetch.py
@@ -14,13 +14,23 @@ from sackigami.constants import (
 @dataclass
 class GameDay:
     season: int
+    """Season or year of the game day.
+
+    This is always the year in which the season begins in,
+    even after January 1st.
+    """
+
     week: int
+    """Week of the game day."""
 
 
 @dataclass
 class SimilarStatLines:
     last_gameday: GameDay
+    """Gameday the a similar stat line occured the last time."""
+
     count: int
+    """How often the similar stat line occured."""
 
 
 def retrieve_complete_team_stats() -> pl.DataFrame:
@@ -33,6 +43,14 @@ def retrieve_complete_team_stats() -> pl.DataFrame:
 
 
 def parse_last_gameday(complete_team_stats: pl.DataFrame) -> GameDay:
+    """Returns the latest/last/current game day.
+
+    Args:
+        complete_team_stats (pl.DataFrame): Complete team stats.
+
+    Returns:
+        GameDay: Last game day in the data.
+    """
     last_season: int = complete_team_stats.select(COL.season.last()).item()
     last_week: int = complete_team_stats.select(COL.week.last()).item()
     return GameDay(last_season, last_week)
@@ -41,6 +59,15 @@ def parse_last_gameday(complete_team_stats: pl.DataFrame) -> GameDay:
 def retrieve_weekly_stats(
     complete_team_stats: pl.DataFrame, gameday: Optional[GameDay] = None
 ) -> pl.DataFrame:
+    """Retrieves the team stats for a given gameday.
+
+    Args:
+        complete_team_stats (pl.DataFrame): Complete team stats.
+        gameday (Optional[GameDay], optional): Game day of which to retrieve data. If none, the latest is chosen. Defaults to None.
+
+    Returns:
+        pl.DataFrame: Weekly team stats.
+    """
 
     gameday_conditional: GameDay = (
         parse_last_gameday(complete_team_stats) if gameday is None else gameday
@@ -53,12 +80,32 @@ def retrieve_weekly_stats(
 
 
 def parse_sack_data(weekly_team_stats: pl.DataFrame) -> pl.DataFrame:
+    """Select relevant columns from given team stats.
+
+    Args:
+        weekly_team_stats (pl.DataFrame): Weekly team stats.
+
+    Returns:
+        pl.DataFrame: Selected columns.
+    """
     return weekly_team_stats.select([data for data in DATA_OF_INTEREST])
 
 
 def find_similar_stat_lines(
     complete_team_stats: pl.DataFrame, stat_line: pl.DataFrame | dict[str, str | int]
 ) -> Optional[SimilarStatLines]:
+    """Finds the amount of and the last time a completely similar stat line occured.
+
+    Args:
+        complete_team_stats (pl.DataFrame): Complete team stats.
+        stat_line (pl.DataFrame | dict[str, str  |  int]): The stat line to look for.
+
+    Raises:
+        TypeError: Stat line is of invalid data type.
+
+    Returns:
+        Optional[SimilarStatLines]: The similar stat line or None of none found.
+    """
 
     sacks_suffered: Optional[int] = None
     sack_yards_lost: Optional[int] = None
